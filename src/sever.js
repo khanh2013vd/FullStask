8295c8d445813f625efd20f3104b155e9b2ff833
khanh2013vd <52021671+khanh2013vd@users.noreply.github.com>
2025-05-05 13:16:03 +0900

    init project

--- src/sever.js
+++ src/sever.js
@@ -18,33 +18,40 @@
 //import express from 'express';
 const express = require('express') //import express
 const path = require('path')
+require('dotenv').config();
 
 const app = express() // tạo express application
-const port = 3000  // init port
+
+//const port = 3000  // init port  //port => hardcode . uat .prop
+const port = process.env.PORT || 8888;
+const hostname = process.env.HOST_NAME;
 
 //config template
 app.set('views', path.join(__dirname, 'views'))
 //app.set('views', './views/');
 app.set('view engine', 'ejs')
 
+//config static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 //khai báo routes
 //req (request), res(response) là 2 object trong môi trường Node.js
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World! test nodemon');
 })
 
 app.get('/abc', (req, res) => {
-    res.send('ABC!')
+    res.send('ABC!');
 })
 
 app.get('/hoc', (req, res) => {
     //res.send('<h1>Hoc FullStask</h1>')
-    res.render('sample.ejs')
+    res.render('sample.ejs');
+    ;
 })
 
 //run server trên port đã khởi tạo trước đấy
 //nạp các thông tin khai báo ở trên rồi chạy (ví dụ như nạp routes)
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(port, hostname, () => {
+    console.log(`Example app listening on port ${port}`);
 })
